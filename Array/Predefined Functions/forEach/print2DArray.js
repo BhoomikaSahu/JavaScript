30a5727236ee8a5d8f4120bf7359f49207963a4e
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2021-10-28 20:00:53 +0530

    Print 2D array using forEach function

---
+++ Array/Predefined Functions/forEach/print2DArray.js
@@ -0,0 +1,9 @@
+const arrayOfArrays = [[1,2,3,4,5],[6,7,8,9,10],[11,22,33,44,55]]
+
+function printArray(element){
+    console.log(element)
+}
+function array(element){
+    element.forEach(printArray)
+}
+arrayOfArrays.forEach(array)
