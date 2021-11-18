260cdbfe3322ff9bbd1e958c68f0f62ebf4ab29e
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2021-11-18 15:01:14 +0530

    map function

---
+++ Array/Predefined Functions/map/map.js
@@ -0,0 +1,15 @@
+// The map() method creates a new array by performing a function on each array element.
+
+// The map() method does not execute the function for array elements without values.
+
+// The map() method does not change the original array.
+
+const array = [11,22,33,44,55];
+
+function printArray(element){
+    console.log(element);        
+    return element;              // It will return each values 
+}
+
+let returnMap = array.map(printArray);
+console.log(returnMap);                     // It will print array    
