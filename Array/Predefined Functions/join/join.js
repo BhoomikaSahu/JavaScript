4eccfa06a2283c956fb5a7fb98589ad5aad2287a
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2021-11-22 13:36:36 +0530

    join function

---
+++ Array/Predefined Functions/join/join.js
@@ -0,0 +1,6 @@
+const array1 = [1,2,3,4,5,6], array2 = [11,22,33,44,55,66], array3 = [111,222,333,444,555,666]
+
+const array4 = array1.concat(array2.concat(array3));
+
+console.log(array4.join(" => "))
+
